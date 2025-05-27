2a9101e2c2c07bf19834a67e86ae6797f54fd29a
SaulFZ <saul.falcon@vincoenergy.com>
2025-05-27 12:46:27 -0600

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -12,7 +12,8 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        // En lugar de probar '/', probamos la página de login que sí devuelve 200
+        $response = $this->get('/login');
 
         $response->assertStatus(200);
     }
