8ddbf55bb8ddfa2d5bf1e35554798d648cbdec18
SaulFZ <saul.falcon@vincoenergy.com>
2026-02-21 21:47:46 -0600

    Update 2025_07_11_171630_create_permissions_table.php

--- database/migrations/2025_07_11_171630_create_permissions_table.php
+++ database/migrations/2025_07_11_171630_create_permissions_table.php
@@ -4,13 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     public function up()
     {
         Schema::create('permissions', function (Blueprint $table) {
             $table->id();
-            $table->string('name'); // Ejemplo: 'view_bonuses'
+            $table->string('name');
             $table->text('description')->nullable();
+            $table->string('system_name')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
