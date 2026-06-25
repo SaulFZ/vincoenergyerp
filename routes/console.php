964e304bfc3f4e76590fcbcb4402c97d9a592669
SaulFZ <saul.falcon@vincoenergy.com>
2026-06-25 10:06:05 -0600

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -84,3 +84,8 @@ Schedule::call(function () {
         );
     }
 })->everyTwoHours();
+
+
+Schedule::call(function () {
+    \Illuminate\Support\Facades\Log::info("¡El reloj de Dokploy está funcionando perfectamente!");
+})->everyMinute();
